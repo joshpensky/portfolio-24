b199c414e7cdad33b9be1b1f498b0af9859d2f5b
Josh Pensky <joshuapensky@gmail.com>
2025-03-19 10:34:04 -0400

    add x-frame-options header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [{ key: "X-Frame-Options", value: "DENY" }],
+      },
+    ];
+  },
+};
 export default nextConfig;
