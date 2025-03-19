700895fb02b84018c3989ae79111d60d3700e771
Josh Pensky <joshuapensky@gmail.com>
2025-03-19 10:18:33 -0400

    add ev monitoring script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "~/lib/cn";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script src="https://monitoring.evervault.app/script.js?id=4543191ab022" />
       <body className={cn(inter.variable, "antialiased")}>{children}</body>
     </html>
   );
